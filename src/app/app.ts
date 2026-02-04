7ae9ab555c36668c86a43152b33090290b09f53a
Gaëtan Rouziès <rouziesg@ardian.com>
2026-02-04 10:15:04 +0700

    Présentation des concepts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,37 @@
 import { Component } from '@angular/core'
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
+  imports: [FormsModule],
 })
-export class App {}
+export class App {
+  // Text Interpolation et Binding
+  title = 'Bienvenue sur Angular !'
+  isChecked = false
+
+  // events
+  count = 0
+  increment() {
+    this.count++
+  }
+
+  logKeyDownEvent(event: KeyboardEvent) {
+    console.log(event)
+  }
+
+  // two-way databinding
+  inputValue = 'Initial value'
+
+  // Control Flow
+  a = 5
+  b = 10
+
+  people = [
+    { id: 1, name: 'Gaëtan', age: 30 },
+    { id: 2, name: 'Jacques', age: 60 },
+    { id: 3, name: 'René', age: 90 },
+  ]
+}
